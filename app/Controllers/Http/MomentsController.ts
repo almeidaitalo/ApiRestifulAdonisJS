d6cbf6b5ac48d5e2db457341ceeaebcf526e226e
Almeida Italo <italoalmeida733@gmail.com>
2024-12-29 17:03:31 -0300

    Upload de imagem criada

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -1,15 +1,35 @@
+import { v4 as uuidv4 } from 'uuid'
+
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 //fazer as manipulações de banco de dados
 import Moment from 'App/Models/Moment'
 
-export default class MomentsController {
+import Application from '@ioc:Adonis/Core/Application'
 
+export default class MomentsController {
+    
+    private validationOptions = {
+        types: ['image'],
+        size: "2mb"
+    }
     //Funcão: inscessão de dados no sistema
     public async store({request, response}: HttpContextContract){
        
         const body = request.body()
         
+        const image = request.file('image', this.validationOptions )
+
+        if(image){
+            const imageName = `${uuidv4()}.${image.extname}`
+
+            await image.move(Application.tmpPath('uploads'), {
+                name: imageName
+            })
+
+            body.image = imageName
+        }
+
         //fazer o inserte no banco de dados
         const moment = await Moment.create(body)
 
